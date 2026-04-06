0726e3ff33ba6711d5059b6dcb445595a1ac32a2
devhu <dshuey@ucsc.edu>
2026-04-06 02:11:04 -0700

    Started IO_Handler class setup

---
+++ Core/Custom/IO_Handler.cpp
@@ -0,0 +1,14 @@
+/*
+ * IO_Handler.cpp
+ *
+ *  Created on: Apr 6, 2026
+ *      Author: devhu
+ */
+
+#include "IO_Handler.hpp"
+
+namespace IO_Handler {
+	namespace Constants {
+
+	}
+}
--- Core/Custom/IO_Handler.hpp
+++ Core/Custom/IO_Handler.hpp
@@ -8,9 +8,25 @@
 #ifndef CUSTOM_IO_HANDLER_HPP_
 #define CUSTOM_IO_HANDLER_HPP_
 
+#include <cstdint>
 // File containing all functions for reading input signals.
 
 namespace IO_Handler {
+
+	class IO_Handler {
+	public:
+		// Input signal sources from the UI. Functions reading from the UI are placed here.
+		bool read_UI_launch_button(); // These implementations are used to finish the ISR functions' conditionals in the SM classes.
+		bool read_UI_fan_toggle_button();
+
+		int* read_UI_stream_selection();
+		// Output signal sources to the UI. It contains a Bluetooth receiver to receive packets of sensor data.
+		// Sensor reading functions are placed here.
+
+	private:
+		QueueHandle_t selected_UI_display_sensors;
+		uint64_t* bluetooth_stream_packet;
+	};
 
 }
 
