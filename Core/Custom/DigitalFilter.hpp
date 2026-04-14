e44403c122587057257cb389bd7a4548259a561d
devhu <dshuey@ucsc.edu>
2026-04-14 01:40:08 -0700

    Renamed RED_DSP

---
+++ Core/Custom/DigitalFilter.hpp
@@ -0,0 +1,18 @@
+/*
+ * RED_DSP.h
+ *
+ *  Created on: Jan 2, 2026
+ *      Author: devhu
+ */
+
+#ifndef CUSTOM_DIGITALFILTER_HPP_
+#define CUSTOM_DIGITALFILTER_HPP_
+
+#include "arm_math.h" // DSP math functions
+
+namespace DSP {
+	void exampleInitFilter(void);
+	void exampleFilterSample(float32_t* input, float32_t* output);
+}
+
+#endif /* CUSTOM_DIGITALFILTER_HPP_ */
